1983c5a728c50af90e2ac27fb2d44b98c9a63c56
hiroshi <hiroshi@iMacHiroshi2.local>
2018-07-22 20:34:18 +0900

    Mod

--- lib/build.sh
+++ lib/build.sh
@@ -1,3 +1,3 @@
 gcc -shared -fPIC -I/usr/local/include  -o stick_sdk.so stick_sdk.c /usr/local/lib/libbcm2835.a
 g++ -I/usr/local/include -c stick_sdk.c
-ar rcs stick_sdk.a stick_sdk.o /usr/local/lib/libbcm2835.a
+ar rcs libstick_sdk.a stick_sdk.o
