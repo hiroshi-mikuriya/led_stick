3dbfb60d87c6d7f1f4cc8a8af4ca195d825fc86a
mikuriya <mickly9@hotmail.com>
2018-07-23 12:51:27 +0900

    mod

--- ruby/test_stick2.rb
+++ ruby/test_stick2.rb
@@ -9,13 +9,6 @@ end
 
 STICK.stop_led_demo
 
-STICK.write_line(0, ([1,0,0] * 32).pack('C*'))
-STICK.write_line(1, ([0,1,0] * 32).pack('C*'))
-STICK.write_line(2, ([0,0,1] * 32).pack('C*'))
-STICK.write_line(3, ([1,1,0] * 32).pack('C*'))
-STICK.write_line(4, ([0,1,1] * 32).pack('C*'))
-STICK.write_line(5, ([1,0,1] * 32).pack('C*'))
-
 loop do
   g = ([0] * 6).pack('C*')
   STICK.get_accel(g)
