9850a6d59e67828649b4c7d4880dc07d59a13b5c
御厨 洋 <mickly9@MacBookAirHiroshi.local>
2018-07-27 15:19:25 +0900

    add button.rb

---
+++ ruby/button.rb
@@ -0,0 +1,14 @@
+require './bcm2835'
+
+if BCM.bcm2835_init.zero?
+  puts 'failed to init bcm2835'
+  exit 1
+end
+
+GPIO = 5 
+
+BCM.bcm2835_gpio_fsel(GPIO, 0)
+BCM.bcm2835_gpio_set_pud(GPIO, 2)
+
+loop { p BCM.bcm2835_gpio_lev(GPIO) }
+
