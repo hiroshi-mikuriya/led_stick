945e7314582f98da0c37794b2451291f09f56549
hiroshi-mikuriya <mickly9@hotmail.com>
2018-08-01 08:36:21 +0000

    use usleep

--- udp/main.cpp
+++ udp/main.cpp
@@ -52,6 +52,7 @@ int main(int argc, const char * argv[])
             mtx.lock();
             show_line(led::width - 1 - line);
             mtx.unlock();
+	    usleep(1);
         }
     });
     namespace asio = boost::asio;
