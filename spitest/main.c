4a260454b4e3d61e97b3e4c7c50d68c7ea0f3d55
御厨 洋 <mickly9@MacBookAirHiroshi.local>
2019-03-12 21:40:30 +0900

    mod sample

--- spitest/main.c
+++ spitest/main.c
@@ -1,14 +1,23 @@
 #include "spi.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
     if (spi_init() != 0) {
         return 1;
     }
-    uint8_t d[] = { 2, 0, 2, 0, 0 }; // stop demo
-    if (spi_write(d, sizeof(d), 0) != 0) {
-        return 1;
+    for (;;) {
+        uint8_t d0[] = { 2, 0, 2, 0, 0 }; // clear
+        if (spi_write(d0, sizeof(d0), 0) != 0) {
+            return 1;
+        }
+        sleep(1);
+        uint8_t d1[] = { 2, 0, 2, 0xFF, 0xFF }; // white
+        if (spi_write(d1, sizeof(d1), 0) != 0) {
+            return 1;
+        }
+        sleep(1);
     }
     if (spi_deinit() != 0) {
         return 1;
