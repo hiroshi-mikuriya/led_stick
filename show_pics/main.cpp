9b6addbd786c08eb459a9059851f3dfacafab6e4
御厨 洋 <mickly9@MacBookAirHiroshi.local>
2019-03-13 07:49:19 +0900

    mod

--- show_pics/main.cpp
+++ show_pics/main.cpp
@@ -6,10 +6,12 @@
 
 namespace
 {
-	void write(cv::Mat m, int lines)
+	const int IMG_WIDTH = COUNT_OF_LINES / 10;
+
+	void write(cv::Mat m, int offset)
 	{
 		cv::Mat img;
-		cv::resize(m, img, cv::Size(lines, COUNT_OF_LED));
+		cv::resize(m, img, cv::Size(IMG_WIDTH, COUNT_OF_LED));
 		for (int x = 0; x < img.cols; ++x) {
 			uint8_t a[COUNT_OF_LED * 3] = { 0 };
 			for (int y = 0; y < img.rows; ++y) {
@@ -18,19 +20,21 @@ namespace
 				a[y * 3 + 1] = bgr[1] / 64;
 				a[y * 3 + 2] = bgr[0] / 64;
 			}
-			if (write_line(x, a) != 0)
+			if (write_line(x + offset, a) != 0)
 				std::exit(1);
 		}
 	}
 
-	void show(int lines)
+	void show(int count)
 	{
-		for(;;){
+		int n = 0; // TODO: ボタン押下でoffsetを更新する。(0 から count - 1 まで)
+		for (;;) {
+			int offset = n * IMG_WIDTH;
 			short a[3] = { 0 };
 			if (get_accel(a) != 0)
 				std::exit(1);
-			int line = (a[1] + 0x8000) * lines / 0x10000;
-			if (show_line(line) != 0)
+			int line = (a[1] + 0x8000) * IMG_WIDTH / 0x10000;
+			if (show_line(offset + line) != 0)
 				std::exit(1);
 			usleep(1000);
 		}
@@ -47,16 +51,18 @@ int main(int argc, const char * argv[])
 		return 1;
 	if (stop_led_demo() != 0)
 		return 1;
-	cv::Mat img = cv::imread(argv[1], 1);
-	if (img.empty()) {
-		std::cerr << "failed to open image file." << std::endl;
-		return 3;
-	}
-	cv::flip(img, img, 1);
-	int const lines = COUNT_OF_LINES;
 	std::cerr << "writing image..." << std::endl;
-	write(img, lines);
+	for (int i = 0; i < argc - 1; ++i) {
+		const char * file = argv[i + 1];
+		cv::Mat img = cv::imread(file, 1);
+		if (img.empty()) {
+			std::cerr << "failed to open " << file << std::endl;
+			return 3;
+		}
+		cv::flip(img, img, 1);
+		write(img, i * IMG_WIDTH);
+	}
 	std::cerr << "complete!" << std::endl;
-	show(lines);
+	show(argc - 1);
 	return 0;
 }
